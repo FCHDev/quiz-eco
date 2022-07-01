b50c22a6b23b6b8c8157f48ad615944f3c9fe855
FCHDev <102167413+FCHDev@users.noreply.github.com>
2022-07-01 21:35:18 +0200

    first commit

---
+++ src/questions_database.js
@@ -0,0 +1,16 @@
+export const questions = [
+  {
+    id: "01",
+    nom: "Quelle est la couleur de la Maison Blanche ?",
+    module: "Economie",
+    incorrectAnswers: ["Bleu", "Violet", "Vert"],
+    correctAnswer: "Blanc",
+  },
+  {
+    id: "02",
+    nom: "Où sont les femmes ?",
+    module: "Economie",
+    incorrectAnswers: ["Au Parc Astérix", "Au Mc Do", "En cuisine !"],
+    correctAnswer: "Dans ton cul",
+  },
+];
